ca3524ca486c4732a59d2e01d1e51eb3fa3f8bca
vitalii <vitalii@MacBook-Pro-vitalii.local>
2023-01-30 11:34:54 +0200

    add auth_screen

--- lib/screens/auth/screen/auth_screen.dart
+++ lib/screens/auth/screen/auth_screen.dart
@@ -21,6 +21,8 @@ class AuthScreen extends StatefulWidget {
 class _AuthScreenState extends State<AuthScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(),
+    );
   }
 }
